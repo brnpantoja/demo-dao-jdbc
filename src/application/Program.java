d25bff04bd72c1d0d664caa9cbe7b6b050c33ba9
brnpantoja <treinadorbrunopantoja@gmail.com>
2022-08-17 00:52:48 -0300

    Update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -33,5 +33,11 @@ public class Program {
         sellerDao.insert(newSeller);
         System.out.println("Inserted! New id = " + newSeller.getId());
 
+        System.out.println("\n==== TEST 5: seller findAll ====");
+        seller = sellerDao.findById(1);
+        seller.setName("Martha Waine");
+        sellerDao.update(seller);
+        System.out.println("Update completed");
+
     }
 }
